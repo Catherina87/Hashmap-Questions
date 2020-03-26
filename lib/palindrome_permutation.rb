6e30c9904015fefb2ebc96d49e4607623d936fee
Catherina <catherine.pchelina@gmail.com>
2020-03-26 10:06:38 -0700

    Implement palindrome_permutation method

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,19 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  hash = {}
+  string.length.times do |i|
+    if !hash.has_key?(string[i])
+      hash[string[i]] = 1
+    else
+      hash[string[i]] += 1
+    end
+  end
+
+  result = hash.select { |key, value| value.odd? }
+
+  if result.length > 1
+    return false
+  else
+    return true
+  end
 end
