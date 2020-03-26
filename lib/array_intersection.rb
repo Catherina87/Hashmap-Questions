6f9eb04471df7b9e57ca227afc4673fdef1eea69
Catherina <catherine.pchelina@gmail.com>
2020-03-26 10:04:28 -0700

    Implement array_intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,17 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  hash = {}
+
+  list1.each do |num|
+    hash[num.to_s] = 1
+  end
+
+  intersection = []
+
+  list2.each do |num|
+    if hash.has_key?(num.to_s)
+      intersection << num
+    end
+  end
+
+  return intersection
 end
