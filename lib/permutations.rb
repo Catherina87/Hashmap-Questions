80a23876cdcbfcdf2f1c2a82ebc2dde655c13742
Catherina <catherine.pchelina@gmail.com>
2020-03-26 10:05:44 -0700

    Implement permutations method

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,31 @@
-
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  if string1 == "" && string2 == ""
+    return true
+  end
+
+  hash = {}
+
+  string1.length.times do |i|
+    if !hash.has_key?(string1[i])
+      hash[string1[i]] = 1
+    else
+      hash[string1[i]] += 1
+    end
+  end
+
+  string2.length.times do |i|
+    if hash[string2[i]] == 0 || !hash.has_key?(string2[i])
+      return false
+    else
+      hash[string2[i]] -= 1
+    end
+  end
+
+  result = hash.select { |key, value| value > 0 }
+
+  if result.length > 0
+    return false
+  else
+    return true
+  end
 end
